a952b51222ad0437284052a206afa459ab1b5790
Youssef ELASERY <joseph.elasery@gmail.com>
2022-12-07 08:42:29 +0100

    PEP8

--- Project/train.py
+++ Project/train.py
@@ -15,8 +15,12 @@ max_depth = None if sys.argv[4] == 'None' else int(sys.argv[4])
 df = pd.read_csv('./dataset/PS_log.csv')
 
 df.drop('Unnamed: 0', axis=1, inplace=True)
-df = df.rename(columns={'oldbalanceOrg':'oldBalanceOrig', 'newbalanceOrig':'newBalanceOrig', \
-                        'oldbalanceDest':'oldBalanceDest', 'newbalanceDest':'newBalanceDest'})
+df = df.rename(
+    columns={
+        'oldbalanceOrg': 'oldBalanceOrig',
+        'newbalanceOrig': 'newBalanceOrig',
+        'oldbalanceDest': 'oldBalanceDest',
+        'newbalanceDest': 'newBalanceDest'})
 
 X = df.loc[(df.type == 'TRANSFER') | (df.type == 'CASH_OUT')]
 
@@ -24,22 +28,22 @@ Y = X['isFraud']
 del X['isFraud']
 
 # Eliminate columns shown to be irrelevant for analysis in the EDA
-X = X.drop(['nameOrig', 'nameDest', 'isFlaggedFraud'], axis = 1)
+X = X.drop(['nameOrig', 'nameDest', 'isFlaggedFraud'], axis=1)
 
 # Binary-encoding of labelled data in 'type'
 X.loc[X.type == 'TRANSFER', 'type'] = 0
 X.loc[X.type == 'CASH_OUT', 'type'] = 1
-X.type = X.type.astype(int) # convert dtype('O') to dtype(int)
+X.type = X.type.astype(int)  # convert dtype('O') to dtype(int)
 
-X.loc[(X.oldBalanceDest == 0) & (X.newBalanceDest == 0) & (X.amount != 0), \
+X.loc[(X.oldBalanceDest == 0) & (X.newBalanceDest == 0) & (X.amount != 0),
       ['oldBalanceDest', 'newBalanceDest']] = - 1
 
-X.loc[(X.oldBalanceOrig == 0) & (X.newBalanceOrig == 0) & (X.amount != 0), \
+X.loc[(X.oldBalanceOrig == 0) & (X.newBalanceOrig == 0) & (X.amount != 0),
       ['oldBalanceOrig', 'newBalanceOrig']] = -2
 
 X['errorBalanceOrig'] = X.newBalanceOrig + X.amount - X.oldBalanceOrig
 X['errorBalanceDest'] = X.oldBalanceDest + X.amount - X.newBalanceDest
-trainX, testX, trainY, testY = train_test_split(X, Y, test_size = 0.2)
+trainX, testX, trainY, testY = train_test_split(X, Y, test_size=0.2)
 
 with mlflow.start_run():
     cls = RandomForestClassifier(
@@ -63,7 +67,7 @@ with mlflow.start_run():
     log_param("max_samples", max_samples)
     log_param("max_features", max_features)
     log_param("max_depth", max_depth)
-    
+
     log_metric("Accuracy", accuracy)
     log_metric("Precision", precision)
     log_metric("Recall", recall)
